abbc95bea560ab172033bbf1fb234223a4ff2823
pauvel <paulsystem32@gmail.com>
2025-01-10 13:11:06 -0700

    Datatype: Int example & gitignore added

---
+++ datatypes/TiposDeDatos.java
@@ -0,0 +1,8 @@
+class TiposDeDatos {
+  public static void main(String args[]){
+    int edad = 18;
+    System.out.println("Mi edad es: " + edad);
+    edad = 19;
+    System.out.println("[updated] Mi edad es: " + edad);
+  }  
+}
