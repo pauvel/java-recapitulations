1643e024c3660ca0c4fa56e09654a18cff284629
pauvel <paulsystem32@gmail.com>
2025-01-11 14:28:41 -0700

    Added section 3: Classes & Objects
    If you will use the classes with a package declaration then compile:
    	javac section3/classes/Main.java section3/classes/Persona.java
    Run:
    	java section3.classes.Main
    
    Persona doesn't need to be imported on Main because they are
    under the same package this time.

---
+++ section3/classes/Main.java
@@ -0,0 +1,10 @@
+// package section3.classes;
+
+// import classes.Persona;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hey!");
+        Persona persona1 = new Persona();
+    }
+}
---
+++ section3/classes/Persona.java
@@ -0,0 +1,19 @@
+// package section3.classes;
+
+public class Persona {
+
+    // Attributes.
+    private String nombre;
+    private short edad;
+    private char genero;
+
+    // Methods.
+
+    private void imprimirInfo(){
+        System.out.println("Nombre: "+ this.nombre);
+        System.out.println("Genero: "+ this.genero);
+        System.out.println("Edad: "+ this.edad);
+    }
+
+
+}
