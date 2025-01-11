0e0562b45c09f7900c5643a3275f35ed21701cfa
pauvel <paulsystem32@gmail.com>
2025-01-10 22:20:57 -0700

    For loop

---
+++ section2/loops/ForLoop.java
@@ -0,0 +1,14 @@
+class ForLoop {
+    public static void main(String args[]){
+
+        for(int i = 0; i<=10; i++){
+            System.out.println("Imprimiendo tabla del " + i);
+            for(int j = 0; j<=10 ; j++){
+                System.out.println(i + "x" + j + "=" + i*j );
+            }
+        }
+
+
+
+    }
+}
