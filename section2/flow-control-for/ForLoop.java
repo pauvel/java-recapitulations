468f0d63871b626f65f5845aa2a6bd151235af2d
pauvel <paulsystem32@gmail.com>
2025-01-11 10:55:18 -0700

    Nested for with tags

--- section2/flow-control-for/ForLoop.java
+++ section2/flow-control-for/ForLoop.java
@@ -1,14 +1,13 @@
 class ForLoop {
     public static void main(String args[]){
 
-        for(int i = 0; i<=10; i++){
+        outer: for(int i = 0; i<=10; i++){
             System.out.println("Imprimiendo tabla del " + i);
-            for(int j = 0; j<=10 ; j++){
+           inner: for(int j = 0; j<=10 ; j++){
                 System.out.println(i + "x" + j + "=" + i*j );
+                if(j ==  9 && i == 9)
+                    break outer;
             }
         }
-
-
-
     }
 }
