c1ed559eabc075a003e451bb1eca40821d75f8d1
pauvel <paulsystem32@gmail.com>
2025-01-13 11:37:41 -0700

    Cat instance of dog compilation error

--- section3/classes/InheritanceExample.java
+++ section3/classes/InheritanceExample.java
@@ -6,6 +6,8 @@ public class InheritanceExample {
         Animal animal = new Animal("NO_NAME");
         Dog dog = new Dog();
         Cat cat = new Cat();
+//        System.out.println(cat instanceof Dog);
+//        Aunque las clases Cat y Dog comparten la misma clase base (Animal), el operador instanceof verifica si el tipo de un objeto puede convertirse al tipo especificado. Si el compilador determina en tiempo de compilación que no hay una posible relación (ni directa ni indirecta) entre los tipos, genera un error.
 
         System.out.println(animal instanceof Animal);
         System.out.println(dog instanceof Animal);
