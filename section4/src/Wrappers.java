1b76a49fe7f80ba1b1dd019c0e83ef75d9bc6af9
pauvel <paulsystem32@gmail.com>
2025-01-28 16:08:39 -0700

    Wrapper classes.

---
+++ section4/src/Wrappers.java
@@ -0,0 +1,9 @@
+public class Wrappers {
+    public static void main(String[] args) {
+        int x = 10; // Primitive
+        Integer y = 10;
+        System.out.println(x);
+        System.out.println(y.floatValue());
+
+    }
+}
