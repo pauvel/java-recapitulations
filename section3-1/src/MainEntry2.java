5dd542d9ad18b0df0176b6537f39ad426b5dc36f
pauvel <paulsystem32@gmail.com>
2025-01-28 11:21:46 -0700

    Enums

--- section3-1/src/MainEntry2.java
+++ section3-1/src/MainEntry2.java
@@ -1,6 +1,14 @@
+import enums.PopcornFlavors;
+
 public class MainEntry2 {
     public static void main(String[] args) {
         MainEntry mainEntry = new MainEntry();
         mainEntry.executeFooProtected();
+        PopcornFlavors caramel = PopcornFlavors.CARAMEL;
+
+        System.out.println(caramel.name());
+        System.out.println(caramel.getGenericName());
+        System.out.println(caramel.getPrice());
+
     }
 }
---
+++ section3-1/src/enums/PopcornFlavors.java
@@ -0,0 +1,23 @@
+package enums;
+
+public enum PopcornFlavors {
+    BBQ("barbique", 2.43f),
+    CHEETOS("Cheese cheetos", 3.12f),
+    CARAMEL("candy", 1.40f);
+
+    private final String genericName;
+    private final float price;
+
+    PopcornFlavors(String genericName, float price){
+        this.genericName = genericName;
+        this.price = price;
+    }
+
+    public String getGenericName() {
+        return genericName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
