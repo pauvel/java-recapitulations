9ff4643a3f50359b551861d5d80cdf370297f26d
pauvel <paulsystem32@gmail.com>
2025-01-29 20:56:55 -0700

    Collections: ArrayList class

---
+++ section4/src/CollectionsExamples.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class CollectionsExamples {
+
+    public static void main(String[] args) {
+        ArrayList<String> lst = new ArrayList<>();
+        lst.add("firstStr");
+        lst.add("secondStr");
+
+        System.out.println(lst.size());
+
+        System.out.println(lst);
+
+    }
+
+}
