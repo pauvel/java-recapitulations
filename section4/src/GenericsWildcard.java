e56efe93e68c1d35f8b1562923797c053fbc121a
pauvel <paulsystem32@gmail.com>
2025-02-01 12:15:22 -0700

    Generics without wildcard

---
+++ section4/src/GenericsWildcard.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class GenericsWildcard {
+
+    static Double sum(List<Number> nums){
+
+        Double s = 0.0;
+        for(Number element : nums){
+            s += element.doubleValue();
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Double x = GenericsWildcard.sum(
+                Arrays.asList(
+                        1,
+                        200,
+                        new Integer(984),
+                        new Float(52.76),
+                        new Double(91.33)
+                )
+        );
+
+        System.out.println("Sum = " + x);
+    }
+
+}
